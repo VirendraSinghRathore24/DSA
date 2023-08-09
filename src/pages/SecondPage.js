62d771d03889b81f3b687baa028b95dcda0bf2e0
VirendraSinghRathore24 <virendrasinghrathore24@gmail.com>
2023-08-09 18:44:05 +0530

    adj1

--- src/pages/SecondPage.js
+++ src/pages/SecondPage.js
@@ -30,7 +30,7 @@ function HomePage() {
        
         <div>
         <p className='text-md font-light py-3 px-10'><strong className='font-semibold text-richblack-700'>To Master in DSA.. Pattern based problems </strong></p>
-            <div className='grid xs:grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 2xl:grid-cols-4 px-20 py-5 gap-x-10 gap-y-10'>
+            <div className='grid xs:grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4 2xl:grid-cols-4 px-10 py-5 gap-x-10 gap-y-10'>
             {
                 posts.map((post, index) => (
                     <Card key={index} post={post}/>
