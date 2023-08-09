01b88c6d17d7b50b464f67e5edb2d8e0734c36e3
VirendraSinghRathore24 <virendrasinghrathore24@gmail.com>
2023-08-09 19:11:09 +0530

    adj4

--- src/pages/Content.js
+++ src/pages/Content.js
@@ -28,8 +28,8 @@ function Content() {
 
     
     <div className='pointer-events-none'>
-        <div>
-            <div className='flex flex-col gap-y-5 ml-20 mt-10 font-semibold'>
+        <div >
+            <div className='flex flex-col gap-y-5 ml-10 mt-10 font-semibold'>
             <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
                 <div className="inline-block min-w-full py-2 sm:px-6 lg:px-8">
                 <div className="overflow-hidden">
