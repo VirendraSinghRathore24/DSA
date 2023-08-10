e2560bdf955d582635b18b2090ded7d146c0a957
VirendraSinghRathore24 <virendrasinghrathore24@gmail.com>
2023-08-10 11:25:56 +0530

    adj7

--- src/pages/Card.js
+++ src/pages/Card.js
@@ -6,18 +6,19 @@ function Card({post}) {
   return (
     <div>
          <div className='relative pointer-events-auto '>
+         <NavLink to={`/${post.title.replaceAll(" ", "-")}`}>
                 <img src="https://res.cloudinary.com/dixqxdivr/image/upload/v1691590733/woodbg_h94qy0.webp" className='w-[320px] h-[280px] rounded-md' loading='lazy'/>
                     
                 <div className='absolute text-stone-950 bottom-0 px-4 text-left'>
                     <p className='text-[30px] font-semibold'>{post.title}</p>
-                    <p className='w-[280px]'>{post.description}</p>
                     <div className='py-10'>
-                    <NavLink to={`/${post.title.replaceAll(" ", "-")}`}>
+                    
                         <button className='bg-blue-500 h-10 w-[100px] font-semibold rounded-md text-white hover:scale-110 transition duration-300 ease-in'
                         >Explore</button>
-                        </NavLink>
+                        
                     </div>
                 </div>
+                </NavLink>
             </div>  
     </div>
   )
