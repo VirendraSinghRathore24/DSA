ce24e021432700389e63f3c6673f53540d686126
VirendraSinghRathore24 <virendrasinghrathore24@gmail.com>
2023-08-16 23:21:04 +0530

    links1211

--- src/pages/DesignPage.js
+++ src/pages/DesignPage.js
@@ -29,16 +29,17 @@ function DesignPage() {
     
     <div>
         <div >
-            <div className='flex flex-col gap-y-2 ml-1 mt-10 font-semibold'>
+            <div className='flex flex-col gap-y-2 ml-1 mt-6 font-semibold'>
             <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
                 <div className="inline-block min-w-full py-2 sm:px-6 lg:px-8">
                 <div className="overflow-hidden">
                     <table className="min-w-full text-left text-sm font-light">
                     <thead className="border-b font-medium dark:border-neutral-500">
                         <tr>
-                        <th scope="col" className="px-6 py-2">#</th>
-                        <th scope="col" className="px-6 py-2">Problem</th>
-                        <th scope="col" className="px-6 py-2">Practice</th>
+                        <th scope="col" className="px-1 py-2"></th>
+                        <th scope="col" className="px-1 py-2">#</th>
+                        <th scope="col" className="px-1 py-2">Problem</th>
+                        <th scope="col" className="px-2 py-2">Practice</th>
                         <th scope="col" className="px-6 py-2">Solution</th>
                         <th scope="col" className="px-6 py-2">Notes</th>
                         </tr>
@@ -48,10 +49,10 @@ function DesignPage() {
                     posts.map((post, index) => (
                         
                             <tr className="border-b dark:border-neutral-500">
-                         
-                                <td className="whitespace-nowrap px-6 py-2 font-medium">{index + 1} .</td>
-                                <td className="whitespace-wrap text-[16px] font-medium px-6 py-2">{post.title}</td>
-                                <td className='px-6 py-2 w-[40px] h-[40px]'>
+                                <td><input type='checkbox' className='w-[25px] h-[25px]'></input></td>
+                                <td className="whitespace-nowrap px-1 py-2 font-medium">{index + 1} .</td>
+                                <td className="whitespace-wrap text-[16px] font-medium px-1 py-2">{post.title}</td>
+                                <td className='px-2 py-2 w-[40px] h-[40px]'>
                                     <a href={post.practiceurl} target="_blank" rel="noreferrer">
                                         <img src="https://res.cloudinary.com/dixqxdivr/image/upload/v1691844715/leetcode_qjndoy.png" className='w-[40px] h-[40px] rounded-md' loading='lazy'/>
                                     </a>
