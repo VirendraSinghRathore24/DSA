aece32471a22efeef64cf4fd24a78543b680763e
VirendraSinghRathore24 <virendrasinghrathore24@gmail.com>
2023-08-16 18:55:04 +0530

    links121

--- src/pages/DesignPage.js
+++ src/pages/DesignPage.js
@@ -29,18 +29,18 @@ function DesignPage() {
     
     <div>
         <div >
-            <div className='flex flex-col gap-y-5 ml-1 mt-10 font-semibold'>
+            <div className='flex flex-col gap-y-2 ml-1 mt-10 font-semibold'>
             <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
                 <div className="inline-block min-w-full py-2 sm:px-6 lg:px-8">
                 <div className="overflow-hidden">
                     <table className="min-w-full text-left text-sm font-light">
                     <thead className="border-b font-medium dark:border-neutral-500">
                         <tr>
-                        <th scope="col" className="px-6 py-4">#</th>
-                        <th scope="col" className="px-6 py-4">Problem</th>
-                        <th scope="col" className="px-6 py-4">Practice</th>
-                        <th scope="col" className="px-6 py-4">Solution</th>
-                        <th scope="col" className="px-6 py-4">Notes</th>
+                        <th scope="col" className="px-6 py-2">#</th>
+                        <th scope="col" className="px-6 py-2">Problem</th>
+                        <th scope="col" className="px-6 py-2">Practice</th>
+                        <th scope="col" className="px-6 py-2">Solution</th>
+                        <th scope="col" className="px-6 py-2">Notes</th>
                         </tr>
                     </thead>
             <tbody>
@@ -49,19 +49,19 @@ function DesignPage() {
                         
                             <tr className="border-b dark:border-neutral-500">
                          
-                                <td className="whitespace-nowrap px-6 py-4 font-medium">{index + 1} .</td>
-                                <td className="whitespace-wrap text-[16px] font-medium px-6 py-4">{post.title}</td>
-                                <td className='px-6 py-4 w-[40px] h-[40px]'>
+                                <td className="whitespace-nowrap px-6 py-2 font-medium">{index + 1} .</td>
+                                <td className="whitespace-wrap text-[16px] font-medium px-6 py-2">{post.title}</td>
+                                <td className='px-6 py-2 w-[40px] h-[40px]'>
                                     <a href={post.practiceurl} target="_blank" rel="noreferrer">
                                         <img src="https://res.cloudinary.com/dixqxdivr/image/upload/v1691844715/leetcode_qjndoy.png" className='w-[40px] h-[40px] rounded-md' loading='lazy'/>
                                     </a>
                                  </td>
-                                <td className='px-6 py-4 w-[40px] h-[40px]'> 
+                                <td className='px-6 py-2 w-[40px] h-[40px]'> 
                                     <a href={post.youtubeurl} target="_blank" rel="noreferrer">
                                     <img src="https://res.cloudinary.com/dixqxdivr/image/upload/v1688707836/youtube-1837872_1280_oicf29.png" className='w-[40px] h-[40px] rounded-md' loading='lazy'/>
                                     </a>
                                 </td>
-                                <td className='px-6 py-4 w-[40px] h-[40px]'> 
+                                <td className='px-6 py-2 w-[40px] h-[40px]'> 
                                    
                                     <img src="https://res.cloudinary.com/dixqxdivr/image/upload/v1692189663/notes1_apfc9m.png" className='w-[40px] h-[40px] rounded-md' loading='lazy'/>
                                     
