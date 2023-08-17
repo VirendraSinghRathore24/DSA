081d4a602a0e29fc157639f2787e71ace6167208
VirendraSinghRathore24 <virendrasinghrathore24@gmail.com>
2023-08-17 18:13:26 +0530

    abo1

--- src/pages/DataStructures.js
+++ src/pages/DataStructures.js
@@ -28,7 +28,7 @@ function DataStructures() {
     <div className='pointer-events-none'>
         <div>
         <p className='text-md font-light py-3 px-3'><strong className='font-semibold text-richblack-700'>Data Structures Pattern based problems </strong></p>
-            <div className='grid xs:grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4 2xl:grid-cols-4 px-10 py-5 gap-x-10 gap-y-10'>
+            <div className='grid xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 2xl:grid-cols-5 px-10 py-5 gap-x-10 gap-y-10'>
             {
                 posts.map((post, index) => (
                     <Card key={index} post={post}/>
